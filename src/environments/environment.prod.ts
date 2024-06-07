8c16229cdbd96110af6525f9ae29b14b767435fe
Julian Camacho <jdacamacho@unicauca.edu.co>
2024-06-07 17:03:27 -0500

    firebase instalado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD5FegOZaA9xTil_b1xQYkRP6qIInuUpH4",
+    authDomain: "productsv1.firebaseapp.com",
+    projectId: "productsv1",
+    storageBucket: "productsv1.appspot.com",
+    messagingSenderId: "379370428512",
+    appId: "1:379370428512:web:87a1344775de25eb424b05"
+  }
 };
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
@@ -6,6 +6,11 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 if (environment.production) {
   enableProdMode();
@@ -15,6 +20,6 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
-    provideRouter(routes),
+    provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"productsv1","appId":"1:379370428512:web:87a1344775de25eb424b05","storageBucket":"productsv1.appspot.com","apiKey":"AIzaSyD5FegOZaA9xTil_b1xQYkRP6qIInuUpH4","authDomain":"productsv1.firebaseapp.com","messagingSenderId":"379370428512"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideAnalytics(() => getAnalytics())), ScreenTrackingService, UserTrackingService, importProvidersFrom(provideFirestore(() => getFirestore())), importProvidersFrom(provideStorage(() => getStorage())),
   ],
 });
